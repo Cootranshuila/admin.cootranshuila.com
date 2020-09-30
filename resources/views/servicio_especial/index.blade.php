b2d3e900a0f53070146092d5cf5b7c0ff7c21537
Leiner Ortega <leicortega@gmail.com>
2020-09-30 09:59:15 -0500

    Correcion temporal de servicio_especial/index

--- resources/views/servicio_especial/index.blade.php
+++ resources/views/servicio_especial/index.blade.php
@@ -8,12 +8,12 @@
 @section('content')
 
 <div class="section-body">
-    
+
     <div class="col-lg-12">
 
         @if (session()->has('creado') && session('creado') == 1)
             <div class="alert alert-icon alert-success col-12" role="alert">
-                <i class="fe fe-check mr-2" aria-hidden="true"></i> Contrato creado correctamente 
+                <i class="fe fe-check mr-2" aria-hidden="true"></i> Contrato creado correctamente
             </div>
             <script> window.open("/servicio-especial/ver/{{ session('contrato_id') }}", "_blank"); </script>
         @endif
@@ -30,11 +30,11 @@
                     <table class="table table-hover table-vcenter table_custom spacing5 text-nowrap mb-0">
                         <thead>
                             <tr>
-                                <th>#</th>                                    
-                                <th>NOMBRE</th>                                    
-                                <th>CEDULA</th>                                    
-                                <th>NOMBRE CONTRATANTE</th>                                    
-                                <th>FECHA</th>                                    
+                                <th>#</th>
+                                <th>NOMBRE</th>
+                                <th>CEDULA</th>
+                                <th>NOMBRE CONTRATANTE</th>
+                                <th>FECHA</th>
                                 <th>No. INTERNO</th>
                                 <th>PLACA</th>
                                 <th class="text-center">Acciones</th>
@@ -44,13 +44,13 @@
                             @foreach ($contratos as $contrato)
                                 <tr>
                                     <td>{{ $contrato->id }}</td>
-                                    <td>{{ $contrato->user->name }}</td>
+                                    <td>{{ $contrato->user->name ?? '' }}</td>
                                     <td>{{ $contrato->identificacion }}</td>
                                     <td>{{ $contrato->contratoRespoNombre }}</td>
                                     <td>{{ $contrato->contratoFechaRealizado }}</td>
                                     <td>{{ $contrato->contratoNumeroInterno }}</td>
                                     <td>{{ $contrato->contratoPlaca }}</td>
-                                    <td class="text-center">                                            
+                                    <td class="text-center">
                                         <button type="button" class="btn btn-primary btn-sm" title="Edit"><i class="fa fa-edit"></i></button>
                                         <button type="button" class="btn btn-danger btn-sm" title="Delete"><i class="fa fa-trash-o"></i></button>
                                     </td>
@@ -60,12 +60,12 @@
                     </table>
 
                     {{ $contratos->links() }}
-                    
+
                 </div>
             </div>
         </div>
     </div>
 
 </div>
-    
+
 @endsection
